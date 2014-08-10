1f4fdacf35a8d877e030817d1963fab23ee2894a
chdtcktt <chdtcktt@gmail.com>
2014-08-10 12:12:35 -0700

    setting up form

---
+++ AzureAestheticsCMS/Controllers/MailingSurfaceController.cs
@@ -0,0 +1,37 @@
+﻿using AzureAestheticsCMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace AzureAestheticsCMS.Controllers
+{
+    public class MailingSurfaceController : SurfaceController
+    {
+        //
+        // GET: /MailingSurface/
+
+        public ActionResult Index()
+        {
+            return PartialView("MailingFormPartial",new MailingViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult FormPost(MailingViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks");
+            }
+            else
+            {
+               return RedirectToCurrentUmbracoPage();
+            }
+
+            
+        }
+
+    }
+}
---
+++ AzureAestheticsCMS/Models/MailingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AzureAestheticsCMS.Models
+{
+    public class MailingViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Opps! You forgot to enter an email address")]
+        [RegularExpression(".+\\@.+\\..+",
+        ErrorMessage = "Aww Man! It has to be a valid email address.")]
+        public string Email { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
